d6252078df7550bc13d40787e1e4b76f17d791ac
Gary Wu <s19003045@gmail.com>
2019-10-30 00:04:38 +0800

    set up routes, controller and views for front-end

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const exphbs = require('express-handlebars')
 const port = 3000
 
 
-app.engine('handlebars', exphbs())
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 
-app.get('/', (req, res) => {
-  res.send('This is index page')
-})
 app.listen(port, () => {
   console.log(`Express server listen on port:${port}`)
 })
+
+require('./routes')(app)
