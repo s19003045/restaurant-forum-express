ace7ce172a5c22b3b02752ec4298636b598efbdc
Gary Wu <s19003045@gmail.com>
2019-10-30 00:28:13 +0800

    set up connection to mysql with sequelize

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
-
+const db = require('./models')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`Express server listen on port:${port}`)
 })
 
