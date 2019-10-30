430805764f707965867521e54f29682872ff2349
Gary Wu <s19003045@gmail.com>
2019-10-30 12:59:22 +0800

    Set up signup page and signup feature

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
 const db = require('./models')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
+const userController = require('../controllers/userController')
 
 
 module.exports = app => {
@@ -8,4 +9,7 @@ module.exports = app => {
 
   app.get('/admin', (req, res) => { res.redirect('/admin/restaurants') })
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
