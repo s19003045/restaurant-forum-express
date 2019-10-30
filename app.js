248eafbc6467c933c5a053a99a274ecf9bb366a4
Gary Wu <s19003045@gmail.com>
2019-10-31 00:29:09 +0800

    add navbar with user information and signout icon

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
 
+  res.locals.user = req.user
   next()
 })
-
 
 
 app.listen(port, () => {
