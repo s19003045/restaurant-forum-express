12be99541c1b3bbf889cef1a3bac9b4d7af74bd9
Gary Wu <s19003045@gmail.com>
2019-11-01 21:43:53 +0800

    modify port setting in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const port = 3000
+const port = process.env.PORT || 3000
 const db = require('./models')
 const bodyParser = require('body-parser')
 const session = require('express-session')
